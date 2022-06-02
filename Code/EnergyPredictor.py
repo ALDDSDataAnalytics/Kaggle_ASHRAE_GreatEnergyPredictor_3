b6fbc40595c73b06f160c04dec3be242b5331320
Leslie D <88687569+735713038455163@users.noreply.github.com>
2022-06-01 22:37:13 -0400

    Create EnergyPredictor.py

---
+++ Code/EnergyPredictor.py
@@ -0,0 +1,6 @@
+# Import the API key.
+from config import apikey
+# Starting URL  API Call.
+url = "http://api.openweathermap.org/data/2.5/weather?units=Imperial&APPID=" + weather_api_key
+print(url)
+
